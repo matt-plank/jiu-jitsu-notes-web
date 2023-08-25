9572c59e6a1bf3daeba3eba844ef0bd5dbff38ab
matt-plank <matthew_plank@outlook.com>
2023-08-25 23:13:40 +0100

    Added nav bar

--- jiu-jitsu-sim-web/src/App.js
+++ jiu-jitsu-sim-web/src/App.js
@@ -1,5 +1,7 @@
+import NavBar from "./components/navbar";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello, World!</h1>;
+  return <NavBar />;
 }
 
 export default App;
---
+++ jiu-jitsu-sim-web/src/components/navbar.js
@@ -0,0 +1,10 @@
+const NavBar = () => {
+  return (
+    <div className="bg-gray-800 px-5 py-5 text-white flex flex-row justify-center gap-10">
+      <p className="text-white">Learn Techniques</p>
+      <p className="text-white">Learn Routes</p>
+    </div>
+  );
+};
+
+export default NavBar;
