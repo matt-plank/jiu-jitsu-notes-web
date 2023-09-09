51dfb9ce7ce5464510deddfb8829a19925da8296
matt-plank <matthew_plank@outlook.com>
2023-09-09 18:02:31 +0100

    Updated layout for learn positions page

--- jiu-jitsu-sim-web/src/pages/learnPositions.js
+++ jiu-jitsu-sim-web/src/pages/learnPositions.js
@@ -21,22 +21,30 @@ const LearnPositions = () => {
     <>
       <NavBar selected={0} />
 
-      <div className="flex flex-row w-full justify-around h-96 items-center">
-        <SearchableDropdown
-          value={currentPositionName}
-          setValue={setCurrentPositionName}
-          onChange={positionChangeHandler}
-          placeholder="Enter Position"
-          options={options}
-        />
+      <div className="flex flex-row w-full justify-center gap-20 h-96 mt-20">
+        <div>
+          <h2 className="text-2xl">Details</h2>
+          <hr className="h-px my-4 bg-gray-800 border-0" />
+          <SearchableDropdown
+            value={currentPositionName}
+            setValue={setCurrentPositionName}
+            onChange={positionChangeHandler}
+            placeholder="Enter Position"
+            options={options}
+          />
+        </div>
 
         <div className="w-1/3">
           <h2 className="text-2xl">Techniques</h2>
           <hr className="h-px my-4 bg-gray-800 border-0" />
           <div className="bg-gray-100 rounded-md px-4 py-4">
-            <p>{currentPosition?.techniques ?? "No techniques"}</p>
+            {currentPosition?.techniques.map((technique) => (
+              <p>{technique}</p>
+            ))}
             <hr className="h-px my-4 bg-gray-400 border-0" />
-            <p>{currentPosition?.submissions ?? "No submissions"}</p>
+            {currentPosition?.submissions.map((technique) => (
+              <p>{technique}</p>
+            ))}
           </div>
         </div>
       </div>
