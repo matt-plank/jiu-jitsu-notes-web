701dd0f2867e223c8124bcd7c2a6c0e5e996cea6
matt-plank <matthew_plank@outlook.com>
2023-09-09 15:15:20 +0100

    Fixed typo in learn position page styling

--- jiu-jitsu-sim-web/src/pages/learnPositions.js
+++ jiu-jitsu-sim-web/src/pages/learnPositions.js
@@ -33,7 +33,7 @@ const LearnPositions = () => {
         <div className="w-1/3">
           <h2 className="text-2xl">Techniques</h2>
           <hr className="h-px my-4 bg-gray-800 border-0" />
-          <div className="bg-gray-200 rounded-md px-4 py-2">
+          <div className="bg-gray-200 rounded-md px-4 py-4">
             <p>{currentPosition?.techniques ?? "None"}</p>
             <hr className="h-px my-4 bg-gray-400 border-0" />
             <p>{currentPosition?.submissions ?? "None"}</p>
