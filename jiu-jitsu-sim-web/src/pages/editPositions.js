bd080979d149470f203aa016dd90b46ddfabdec3
matt-plank <matthew_plank@outlook.com>
2023-09-10 22:28:07 +0100

    Closes #18. Added position selector for edit positions page

--- jiu-jitsu-sim-web/src/pages/editPositions.js
+++ jiu-jitsu-sim-web/src/pages/editPositions.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
 import NavBar from "../components/navbar";
+import SearchableDropdown from "../components/searchableDropdown";
+import useAllPositions from "../hooks/useAllPositions";
 
 const EditPositions = () => {
-  return <NavBar selected="/edit" />;
+  const positions = useAllPositions();
+  const options = positions.map((position) => position.display_name);
+  const [query, setQuery] = useState("");
+
+  return (
+    <>
+      <NavBar selected="/edit" />
+
+      <div className="flex justify-center">
+        <div className="flex flex-col mt-10 gap-5 w-1/3">
+          <SearchableDropdown
+            className="w-full"
+            placeholder="Enter Position"
+            options={options}
+            value={query}
+            setValue={setQuery}
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default EditPositions;
