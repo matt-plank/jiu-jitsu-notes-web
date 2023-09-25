5d275c533d6e8da529d25064a6f19d40cbee33b9
matt-plank <matthew_plank@outlook.com>
2023-09-25 22:16:02 +0100

    Added warning message to log in page for failed logins

--- jiu-jitsu-notes/src/pages/login.js
+++ jiu-jitsu-notes/src/pages/login.js
@@ -5,12 +5,13 @@ import ActionButton from "../components/actionButton";
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const logIn = async () => {
     const token = await tokenApi.create(username, password);
 
     if (token === "") {
-      alert("Incorrect username or password");
+      setErrorMessage("Incorrect username or password");
       return;
     }
 
@@ -21,22 +22,28 @@ const Login = () => {
   return (
     <>
       <div className="flex flex-col justify-center items-center h-screen">
-        <div className="flex flex-col justify-center items-center gap-5 p-5 rounded-md">
+        <div className="flex flex-col justify-center items-center gap-5 p-5 rounded-md w-80">
           <h1 className="text-5xl font-thin m-5">Log In</h1>
           <input
-            className="p-2 rounded-sm border"
+            className="p-2 rounded-sm border w-full"
             type="text"
             placeholder="Username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
           <input
-            className="p-2 rounded-sm border"
+            className="p-2 rounded-sm border w-full"
             type="password"
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+
+          {errorMessage && (
+            <div className="w-full px-4 py-2 bg-red-500 rounded-sm text-white">
+              {errorMessage}
+            </div>
+          )}
 
           <ActionButton className="w-full" onClick={logIn}>
             Log In
