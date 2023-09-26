caa2b221d7729a038f67be1bd57af1c6a717112f
matt-plank <matthew_plank@outlook.com>
2023-09-26 13:25:58 +0100

    Added hover effect to footer links

--- jiu-jitsu-notes/src/components/footer.js
+++ jiu-jitsu-notes/src/components/footer.js
@@ -2,8 +2,18 @@ const Footer = () => {
   return (
     <div className="w-full bg-gray-800 px-5 py-3 flex gap-10 justify-center items-center text-gray-400 mt-10 text-sm">
       <p>Site by Matt Plank</p>
-      <a href="https://www.linkedin.com/in/matt-t-p/">LinkedIn</a>
-      <a href="https://github.com/matt-plank">GitHub</a>
+      <a
+        href="https://www.linkedin.com/in/matt-t-p/"
+        className="hover:text-white duration-100"
+      >
+        LinkedIn
+      </a>
+      <a
+        href="https://github.com/matt-plank"
+        className="hover:text-white duration-100"
+      >
+        GitHub
+      </a>
     </div>
   );
 };
