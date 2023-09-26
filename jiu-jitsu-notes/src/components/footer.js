c12889cf834d38451d8a19b62e1074957412c1d5
matt-plank <matthew_plank@outlook.com>
2023-09-26 09:28:03 +0100

    Updated footer styling

--- jiu-jitsu-notes/src/components/footer.js
+++ jiu-jitsu-notes/src/components/footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="w-full bg-gray-800 p-5 flex gap-10 justify-center items-center text-gray-100 mt-10">
+    <div className="w-full bg-gray-800 px-5 py-3 flex gap-10 justify-center items-center text-gray-400 mt-10 text-sm">
       <p>Site by Matt Plank</p>
       <a href="https://www.linkedin.com/in/matt-t-p/">LinkedIn</a>
       <a href="https://github.com/matt-plank">GitHub</a>
