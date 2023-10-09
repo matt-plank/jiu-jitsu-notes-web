02a94f0083b0709dd6d15452fdf33e47cc5c7de6
matt-plank <matthew_plank@outlook.com>
2023-10-09 13:15:42 +0100

    Added logo back to nav bar

--- jiu-jitsu-notes/src/components/nav/navbar.js
+++ jiu-jitsu-notes/src/components/nav/navbar.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import { LuLogOut } from "react-icons/lu";
+import { Link } from "react-router-dom";
 import { CSSTransition } from "react-transition-group";
 import tokenStorage from "../../api/tokenStorage";
+import Logo from "../logo/logo";
 import NavPanel from "./navPanel";
 
 const NavBar = ({ username }) => {
@@ -30,10 +32,16 @@ const NavBar = ({ username }) => {
     <>
       <div className="relative">
         <div className="w-full bg-gray-800 p-5 text-white flex gap-5 items-center justify-between select-none z-20 relative">
-          <AiOutlineMenu
-            className="text-2xl cursor-pointer active:scale-75 duration-75"
-            onClick={toggle}
-          />
+          <div className="flex gap-8">
+            <AiOutlineMenu
+              className="text-2xl cursor-pointer active:scale-75 duration-75"
+              onClick={toggle}
+            />
+
+            <Link to="/">
+              <Logo dark />
+            </Link>
+          </div>
 
           <div className="flex gap-5">
             {username}
