4032104184feb79d4350d21212172b45ba5502f4
matt-plank <matthew_plank@outlook.com>
2023-10-09 12:18:05 +0100

    Added  active animation for nav menu button

--- jiu-jitsu-notes/src/components/nav/navbar.js
+++ jiu-jitsu-notes/src/components/nav/navbar.js
@@ -13,7 +13,10 @@ const NavBar = ({ username }) => {
     <>
       <div className="relative">
         <div className="w-full bg-gray-800 p-5 text-white flex gap-5 items-center justify-between select-none">
-          <AiOutlineMenu className="text-2xl" onClick={toggle} />
+          <AiOutlineMenu
+            className="text-2xl cursor-pointer active:scale-75 duration-75"
+            onClick={toggle}
+          />
 
           {username}
         </div>
