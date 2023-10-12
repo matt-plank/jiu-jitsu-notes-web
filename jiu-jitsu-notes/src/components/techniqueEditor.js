2d2eb30265400edc346ef3ecdc484982369beaff
matt-plank <matthew_plank@outlook.com>
2023-10-12 13:25:26 +0100

    Redesigned mobile layout of positions page

--- jiu-jitsu-notes/src/components/techniqueEditor.js
+++ jiu-jitsu-notes/src/components/techniqueEditor.js
@@ -9,42 +9,46 @@ const TechniqueEditor = ({
   setSelectedPosition,
 }) => {
   return (
-    <div className="flex gap-2 overflow-x-auto">
+    <div className="flex flex-wrap gap-2">
       <input
         type="text"
         placeholder="Enter Technique Name"
-        className="min-w-40 p-2 border border-gray-200 rounded-md shadow-sm bg-white !outline-none w-full"
+        className="bg-white p-2 border border-gray-200 rounded-md shadow-sm flex-1"
         value={name}
         onChange={(e) => {
           setName(e.target.value);
         }}
       />
 
-      <select
-        className="min-w-40 border border-gray-200 rounded-md shadow-sm p-2 bg-white w-full"
-        value={toPosition?.toString() ?? ""}
-        onChange={(e) => {
-          setToPosition({
-            id: parseInt(e.target.value),
-          });
-        }}
-      >
-        <option value="">--- None ---</option>
-        {positions.map((position) => (
-          <option value={position.id}>{position.display_name}</option>
-        ))}
-      </select>
+      <div className="flex-1 flex gap-2">
+        <select
+          className="w-full min-w-40 bg-white p-2 border border-gray-200 rounded-md shadow-sm"
+          value={toPosition?.toString() ?? ""}
+          onChange={(e) => {
+            setToPosition({
+              id: parseInt(e.target.value),
+            });
+          }}
+        >
+          <option value="">--- None ---</option>
+          {positions.map((position) => (
+            <option value={position.id}>{position.display_name}</option>
+          ))}
+        </select>
 
-      <ActionButton
-        onClick={() => {
-          if (!toPosition) return;
+        <ActionButton
+          onClick={() => {
+            if (!toPosition) return;
 
-          setSelectedPosition(positions.filter((p) => p.id === toPosition)[0]);
-        }}
-        className="min-w-20"
-      >
-        Go To
-      </ActionButton>
+            setSelectedPosition(
+              positions.filter((p) => p.id === toPosition)[0]
+            );
+          }}
+          className="min-w-max"
+        >
+          Go To
+        </ActionButton>
+      </div>
     </div>
   );
 };
--- jiu-jitsu-notes/src/components/techniqueEditorColumn.js
+++ jiu-jitsu-notes/src/components/techniqueEditorColumn.js
@@ -11,17 +11,19 @@ const TechniqueEditorColumn = ({
     <div className="p-5 bg-white shadow-sm rounded-lg flex flex-col gap-5 w-full">
       <h2 className="text-xl">Techniques</h2>
 
-      {techniques.techniques.map((technique, i) => (
-        <TechniqueEditor
-          position={position}
-          name={technique.name}
-          setName={techniques.setPropertyByIndex(i, "name")}
-          positions={positions.positionList}
-          toPosition={technique.to_position.id}
-          setToPosition={techniques.setPropertyByIndex(i, "to_position")}
-          setSelectedPosition={setSelectedPosition}
-        />
-      ))}
+      <div className="flex flex-col gap-5">
+        {techniques.techniques.map((technique, i) => (
+          <TechniqueEditor
+            position={position}
+            name={technique.name}
+            setName={techniques.setPropertyByIndex(i, "name")}
+            positions={positions.positionList}
+            toPosition={technique.to_position.id}
+            setToPosition={techniques.setPropertyByIndex(i, "to_position")}
+            setSelectedPosition={setSelectedPosition}
+          />
+        ))}
+      </div>
 
       <ActionButton onClick={techniques.newEmptyTechnique}>
         New Technique
